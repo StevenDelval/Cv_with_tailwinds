34aefdf223ffc1c3ee515a80c23f7fd9f4deedac
StevenDsimplon <asteven59220@free.fr>
2022-04-28 16:20:05 +0200

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*.html"],
+
+  theme: {
+    extend: {
+      colors: {
+        'red-perso': '#E94F37',
+        'black-gray':'#1C1C1C',
+      },
+    }
+  }
+  ,
+  plugins: [],
+}
